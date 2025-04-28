b883e050217823c864d242ac494ddaaa7ce89dad
brensangura <sangura.bren@gmail.com>
2025-04-28 20:55:20 +0300

    doms, html and js

---
+++ doms.js
@@ -0,0 +1,13 @@
+//By ID
+const elementID = document.getElementById("elementID");
+
+//By Class Name
+const elementCls = document.getElementsByClassName("my Class");
+
+//By Tag Name
+const paragraphs = document.getElementsByTagName("p");
+
+//By Query Selector
+const element = document.querySelector(".myClass"); // Selects the first element with class "myClass"
+
+const elements = document.querySelectorAll(".myClass"); // Selects all elements with class "myClass"
