ab0b879283b35e4627387b9287542c7262d98290
brensangura <sangura.bren@gmail.com>
2025-04-28 19:16:44 +0300

    loops

---
+++ loops.js
@@ -0,0 +1,42 @@
+//For loops
+//for (initialization; condition; increment/decrement) {
+//    //code to be executed
+//}
+for (let a = 1; a <= 10; a += 2) {
+    console.log(`Counter Num: ${a}`);
+}
+
+
+//while loops
+let count = 2;
+
+while (count < 10) {
+    //console.log(`Counter Num: ${count}`);
+    count ++; // Increment the count by 1
+}
+
+//do while loops
+let number = 10;
+do {
+    console.log(`Counter Num: ${number}`);
+    number -=2; // decrement the count by 1
+}
+
+//For...of Loop
+const fruits = ['apple', 'banana', 'orange', 'Cherry', 'Mango'];
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+
+const greeting = 'Hello World!';
+for (const char of greeting) {
+    console.log(char);
+}
+
+//For...in Loop
+const person = {
+    name: 'John',
+    LastName: 'Doe',
+    age: 30,
+    city: 'New York'
+};
